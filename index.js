4ac57f49d187eb2137e759fc6cecdf123844e3b7
Mohit Bajaj <mohit_b35@hotmail.com>
2020-01-14 15:56:33 +0530

    Autocomplete widget - loose coupling - Empty response handling - Broken image handling - Click to close - Search with autocomplete - Clear results on new search

--- index.js
+++ index.js
@@ -13,9 +13,18 @@ const fetchData = async (searchTerm) => {
 	return response.data.Search;
 }
 
-// fetchData();
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+	<label><b>Search for a movie</b></label>
+	<input type="text" class="input">
+	<div class="dropdown">
+		<div class="dropdown-menu">
+			<div class="dropdown-content results"></div>
+		</div>
+	</div>
+`;
 
-const fetchMovie = async () => {
+/* const fetchMovie = async () => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params: {
 			apikey: '8957c2b8',
@@ -24,12 +33,15 @@ const fetchMovie = async () => {
 	});
 
 	console.log("Response", response.data);
-}
+} */
 
 // fetchMovie();
 
 
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
 
 // Simple search delay
 /*let timeoutId;
@@ -44,17 +56,35 @@ const onInput = (event) => {
 
 const onInput = async (event) => {
 	let movies = await fetchData(event.target.value);
+
+	if(!movies.length) {
+		return;
+	}
+
+	resultsWrapper.innerHTML = '';
+	dropdown.classList.add('is-active');
 	
 	for(let movie of movies){
-		const div = document.createElement('div');
-		div.innerHTML = `
-			<img src="${movie.Poster}"/>
-			<h1>${movie.Title}</h1>
+		const option = document.createElement('a'); 
+
+		const imgSrc = movie.Poster === "N/A" ? '' : movie.Poster;
+
+		option.classList.add('dropdown-item');
+		option.innerHTML = `
+			<img src="${imgSrc}"/>
+			${movie.Title}
 		`;
-		document.querySelector('#target').appendChild(div);
+		resultsWrapper.appendChild(option);
 
 	}
 }
 
 // input.addEventListener('input', onInput);
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', (event) => {
+	if(!root.contains(event.target)) {
+		dropdown.classList.remove('is-active');
+		resultsWrapper.innerHTML = '';
+	}
+});
